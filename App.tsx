efa1e3e6fea0f364fcaeb534aaa0f9346ae8a9bf
jep <juepark42@gmail.com>
2025-08-08 12:29:34 +0900

    Add Account and Auth components; implement user profile management and authentication

--- App.tsx
+++ App.tsx
@@ -7,10 +7,32 @@ import Icon from "react-native-vector-icons/MaterialIcons";
 import { HomeScreen } from "./screens/HomeScreen";
 import { ProfileScreen } from "./screens/ProfileScreen";
 import { EmptyScreenComponent } from "./components/EmptyScreenComponent";
+import { useState, useEffect } from 'react'
+import { supabase } from './lib/supabase'
+import Auth from './components/Auth'
+import Account from './components/Account'
+import { View } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+    const [session, setSession] = useState<Session | null>(null)
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+    }, [])
+
+    return (
+        <View>
+            {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+        </View>
+    )
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#FDFDFD" }}>
       <NavigationContainer>
